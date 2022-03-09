93417f91e2ddc87d2e660d2fde776a9d464fd766
Stella Mbithe <mbithestella0@gmail.com>
2022-03-09 23:17:49 +0300

    initial commit

---
+++ app/src/main/java/com/example/myapplication/Login.kt
@@ -0,0 +1,24 @@
+package com.example.myapplication
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.myapplication.databinding.ActivityLoginBinding
+
+class Login : AppCompatActivity() {
+
+    private lateinit var binding: ActivityLoginBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        supportActionBar?.hide()
+        super.onCreate(savedInstanceState)
+        setContentView(binding.root)
+
+        binding.btnLogin.setOnClickListener {
+            startActivity(Intent(this,Home::class.java))
+        }
+        binding.tvRegister.setOnClickListener {
+            startActivity(Intent(this,Register::class.java))
+        }
+    }
+}
---
+++ app/src/main/java/com/example/myapplication/Register.kt
@@ -0,0 +1,23 @@
+package com.example.myapplication
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.myapplication.databinding.ActivityRegisterBinding
+
+class Register : AppCompatActivity() {
+    private lateinit var binding : ActivityRegisterBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        binding = ActivityRegisterBinding.inflate(layoutInflater)
+        supportActionBar?.hide()
+        super.onCreate(savedInstanceState)
+        setContentView(binding.root)
+
+        binding.btnRegister.setOnClickListener {
+            startActivity(Intent(this,Home::class.java))
+        }
+        binding.tvLogin.setOnClickListener {
+            startActivity(Intent( this,Login::class.java))
+        }
+    }
+}
