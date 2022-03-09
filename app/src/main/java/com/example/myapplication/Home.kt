9b6bd46fa1d6d57f4c3d452258e86a143927a24f
Stella Mbithe <mbithestella0@gmail.com>
2022-03-10 00:07:36 +0300

    add comment

--- app/src/main/java/com/example/myapplication/Home.kt
+++ app/src/main/java/com/example/myapplication/Home.kt
@@ -4,6 +4,10 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.myapplication.databinding.ActivityHomeBinding
 
+/**
+ * this is the home class
+ */
+
 class Home : AppCompatActivity() {
     private lateinit var binding : ActivityHomeBinding
     override fun onCreate(savedInstanceState: Bundle?) {
